d77369c6d9a3fdef3572ecde5ca4b14fc899b466
Michael Welt <michael.welt@posteo.de>
2020-03-18 03:29:24 +0100

    wp parsing

--- src/tests.rs
+++ src/tests.rs
@@ -1,9 +1,18 @@
 use super::*;
-use std::fs::read_to_string;
+use std::fs::{read_to_string, write};
 
 #[test]
 fn test_wp_parser(){
+    assert_eq!(wikipedia_parser::parse("{{abc}}efg{{hij}}"), "efg");
+    assert_eq!(wikipedia_parser::parse("ab[[cd|ef]]hi"), "abcdhi");
+    assert_eq!(wikipedia_parser::parse("ab[[cd|ef]]hi[[jkl]]mn[[o|pq]]rs"), "abcdhijklmnors");
+    assert_eq!(wikipedia_parser::parse("== abcd =="), " abcd ");
+}
+
+// #[test]
+fn test_wp_parser_on_file(){
     let txt = read_to_string("wp.txt")
         .expect("Could not read test file wp.txt."); 
-    assert_eq!(wikipedia_parser::parse(&txt), true);
+    let txt_ = wikipedia_parser::parse(&txt);
+    write("wp_.txt", txt_);
 }
--- src/wikipedia_parser.rs
+++ src/wikipedia_parser.rs
@@ -1,11 +1,21 @@
 use regex::Regex;
 
-pub fn parse(s: &str) -> bool {
+pub fn parse(s: &str) -> String {
 
     lazy_static! {
         static ref RE_CURLY_BRACES: Regex = 
-            Regex::new(r"(\{\{.*\}\})").unwrap();
+            Regex::new(r"(\{\{(?P<c>[^\}]*)\}\})").unwrap();
+        static ref RE_LINK_BRACES: Regex =
+            Regex::new(r"(\[\[(?P<c>File:[^\]|\|]*).*\]\])").unwrap();
+        static ref RE_WP_LINK_BRACES: Regex =
+            Regex::new(r"(\[\[(?P<c>[^\]|\|]*).*\]\])").unwrap();
+        static ref RE_HEADLINE: Regex =
+            Regex::new(r"(==(?P<c>[^=]*)==)").unwrap();
     } 
 
-    RE_CURLY_BRACES.is_match(s)
+    let s = RE_CURLY_BRACES.replace_all(s, "");
+    let s = RE_LINK_BRACES.replace_all(&*s, "");
+    let s = RE_HEADLINE.replace_all(&*s, "$c");
+    RE_WP_LINK_BRACES.replace_all(&*s, "$c").into()
+
 }
