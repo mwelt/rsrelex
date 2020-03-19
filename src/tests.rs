698991beb47c4f9b1c3e743dbbb9f0e483fddc24
Michael Welt <michael.welt@uni-ulm.de>
2020-03-19 19:08:59 +0100

    testing

--- src/tests.rs
+++ src/tests.rs
@@ -3,12 +3,10 @@ use std::fs::{read_to_string, write};
 
 #[test]
 fn test_wp_parser(){
-   
-    let big_test_str = "
-*{{cite journal|last=Antliff|first=Mark |year=1998|title=Cubism, Futurism, Anarchism: The 'Aestheticism' of the Action d'art Group, 1906-1920|journal=Oxford Art Journal|volume=21|issue=2|pages=101–120 |ref=harv|jstor=1360616 |doi=10.1093/oxartj/21.2.99 }}
+
+    let big_test_str = "*{{cite journal|last=Antliff|first=Mark |year=1998|title=Cubism, Futurism, Anarchism: The 'Aestheticism' of the Action d'art Group, 1906-1920|journal=Oxford Art Journal|volume=21|issue=2|pages=101–120 |ref=harv|jstor=1360616 |doi=10.1093/oxartj/21.2.99 }}
 * {{cite book |last=Avrich |first=Paul|title=Anarchist Voices: An Oral History of Anarchism in America |year=1996|publisher=Princeton University Press |isbn=978-0-691-04494-1|ref=harv}}
-* {{cite book |last=Bantman|first=Constance|chapter=The Era of Propaganda by the Deed|pages=371–388|editor1-last=Levy|editor1-first=Carl|editor1-link=Carl Levy (political scientist)|editor2-last=Adams|editor2-first=Matthew S. |title=The Palgrave Handbook of Anarchism|year=2019|publisher=[[Springer Publishing]]|isbn=978-3-319-75620-2|ref=harv}}
-";
+* {{cite book |last=Bantman|first=Constance|chapter=The Era of Propaganda by the Deed|pages=371–388|editor1-last=Levy|editor1-first=Carl|editor1-link=Carl Levy (political scientist)|editor2-last=Adams|editor2-first=Matthew S. |title=The Palgrave Handbook of Anarchism|year=2019|publisher=[[Springer Publishing]]|isbn=978-3-319-75620-2|ref=harv}}";
 
 
     assert_eq!(wikipedia_parser::strip_markup(big_test_str), ""); 
@@ -40,7 +38,7 @@ fn test_wp_parser(){
     assert_eq!(wikipedia_parser::strip_markup("&quot;abc&quot;"), "abc");
 
     // horizontal rules
-    assert_eq!(wikipedia_parser::strip_markup("--------\n"), "");
+    assert_eq!(wikipedia_parser::strip_markup("--------\n"), "\n");
 
     // ;
     assert_eq!(wikipedia_parser::strip_markup("abcd; efg\n"), "abcd; efg\n");
@@ -49,11 +47,12 @@ fn test_wp_parser(){
 
     // lists
     assert_eq!(wikipedia_parser::strip_markup("\n     ** this is an item\n"), "\n");
+    assert_eq!(wikipedia_parser::strip_markup("\n     ; this is an item\n"), "\n");
     assert_eq!(wikipedia_parser::strip_markup("\n** this is an item\n"), "\n");
     assert_eq!(wikipedia_parser::strip_markup("\n## this is an item\n"), "\n");
 }
 
-// #[test]
+#[test]
 fn test_wp_parser_on_file(){
     let txt = read_to_string("wp.txt")
         .expect("Could not read test file wp.txt."); 
--- src/wikipedia_parser.rs
+++ src/wikipedia_parser.rs
@@ -4,33 +4,40 @@ pub fn strip_markup(s: &str) -> String {
 
     lazy_static! {
         static ref REMOVE: Vec<String> =  vec!(
-            "&quot;".into(), "'''".into(), "''".into() 
+            "&quot;".into(), 
+            "'''".into(), 
+            "''".into(),
+        );
+
+        static ref REPLACE: Vec<(&'static str, &'static str)> = vec!(
+            ("&gt;", ">")
         );
 
         static ref REGEX: Vec<(Regex, String)> = vec!(
-            (Regex::new(r"(\n)(\s*(;|[*#:]+)[^\n]*\n)").unwrap(), "$1".into()),
+            // (Regex::new(r"([\n|\^])(\s*(;|[*#:]+)[^\n]*[\n|$])").unwrap(), "".into()),
+            (Regex::new(r"(?m)(^\s*[*#:;]+[^\n]*$)").unwrap(), "".into()),
 
-            // // every wikimarkup {{...}}
-            // (Regex::new(r"(\{\{(?P<c>[^}]*)\}\})").unwrap(), "".into()),
-            // // every wikimarkup {|...|}
-            // (Regex::new(r"(\{\|(?P<c>[^}]*)\|\})").unwrap(), "".into()),
-            // // every File link 
-            // (Regex::new(r"(\[\[(?P<c>File:[^]]*).*\]\])").unwrap(), "".into()),
-            // // every Category link
-            // (Regex::new(r"(\[\[(?P<c>Category:[^]]*).*\]\])").unwrap(), "".into()),
-            // // modify wiki internal links
-            // (Regex::new(r"(\[\[(?P<c>[^]|]*)\|?[^]]*\]\])").unwrap(), "$c".into()),
-            // // headlines
-            // (Regex::new(r"(===== (?P<c>[^=]+) =====)").unwrap(), "".into()),
-            // (Regex::new(r"(==== (?P<c>[^=]+) ====)").unwrap(), "".into()),
-            // (Regex::new(r"(=== (?P<c>[^=]+) ===)").unwrap(), "".into()),
-            // (Regex::new(r"(== (?P<c>[^=]+) ==)").unwrap(), "".into()),
-            // (Regex::new(r"(= (?P<c>[^=]+) =)").unwrap(), "".into()),
+            // every wikimarkup {{...}}
+            (Regex::new(r"(\{\{(?P<c>[^}]*)\}\})").unwrap(), "".into()),
+            // every wikimarkup {|...|}
+            (Regex::new(r"(\{\|(?P<c>[^}]*)\|\})").unwrap(), "".into()),
+            // every File link 
+            (Regex::new(r"(\[\[(?P<c>File:[^]]*).*\]\])").unwrap(), "".into()),
+            // every Category link
+            (Regex::new(r"(\[\[(?P<c>Category:[^]]*).*\]\])").unwrap(), "".into()),
+            // modify wiki internal links
+            (Regex::new(r"(\[\[(?P<c>[^]|]*)\|?[^]]*\]\])").unwrap(), "$c".into()),
+            // headlines
+            (Regex::new(r"(===== (?P<c>[^=]+) =====)").unwrap(), "".into()),
+            (Regex::new(r"(==== (?P<c>[^=]+) ====)").unwrap(), "".into()),
+            (Regex::new(r"(=== (?P<c>[^=]+) ===)").unwrap(), "".into()),
+            (Regex::new(r"(== (?P<c>[^=]+) ==)").unwrap(), "".into()),
+            (Regex::new(r"(= (?P<c>[^=]+) =)").unwrap(), "".into()),
 
-            // // comments
-            // (Regex::new(r"(&lt;!--(?P<c>[^-]*)--&gt;)").unwrap(), "".into()),
-            // // horizontal rules
-            // (Regex::new(r"(\-+\n)").unwrap(), "".into()),
+            // comments
+            (Regex::new(r"(<!--(?P<c>[^-]*)-->)").unwrap(), "".into()),
+            // horizontal rules
+            (Regex::new(r"(?m)(^\-+$)").unwrap(), "".into()),
             
             // (Regex::new(r"(\n)(\s*;[^\n]*\n)").unwrap(), "$1".into()),
             // (Regex::new(r"(\n)(\s*[*#:]+[^\n]*\n)").unwrap(), "$1".into()),
