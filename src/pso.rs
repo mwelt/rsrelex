8c990c7c846387063c08e1fe9680497c142489b0
Michael Welt <michael.welt@uni-ulm.de>
2020-03-31 00:17:58 +0200

    pareto front

--- src/pso.rs
+++ src/pso.rs
@@ -1,16 +1,28 @@
+use rand::Rng;
 use rand::distributions::{Distribution, Uniform};
-use rand_distr::Normal;
 
-#[derive(Debug)]
 pub struct Swarm {
-    pub uniform_distribution: Vec<Uniform<f64>>,
-    pub normal_distribution: Vec<Normal<f64>>,
     pub learning_cognitive: f64,
     pub learning_social: f64,   
     pub inertia: f64,
     pub best: Vec<f64>,
+    pub best_fitness: f64,
     pub bounds: Vec<(f64, f64)>,
-    pub particles: Vec<Particle>
+    pub particles: Vec<Particle>,
+    pub fitness_fn: fn(&[f64]) -> f64
+}
+
+impl ToString for Swarm {
+    fn to_string(&self) -> String {
+        [
+            "Swarm:",
+            "\nlearning_cognitive:", &self.learning_cognitive.to_string(),
+            "\nlearning_social:", &self.learning_social.to_string(),
+            "\ninertia:", &self.inertia.to_string(),
+            "\nbest:", &self.best.iter().map(|x| x.to_string()).collect::<Vec<String>>().join(", "),
+            "\nbeest_fitness:", &self.best_fitness.to_string(),
+        ].join(" ")
+    }
 }
 
 impl Swarm {
@@ -19,7 +31,8 @@ impl Swarm {
         bounds: Vec<(f64, f64)>,
         learning_cognitive: f64,
         learning_social: f64,
-        inertia: f64) -> Swarm {
+        inertia: f64,
+        fitness_fn: fn(&[f64]) -> f64) -> Swarm {
 
         let n: usize = bounds.len();
 
@@ -30,57 +43,79 @@ impl Swarm {
         let mut particles: Vec<Particle> = Vec::with_capacity(num_particles);
 
         for _ in 0..num_particles {
-            particles.push(Particle::new_rnd(n, &uniform_distribution)); 
+            particles.push(Particle::new_rnd(&uniform_distribution)); 
         }
 
         Swarm {
-            uniform_distribution,
-            normal_distribution: bounds.iter()
-                .map(|(l, h)| Normal::new(0f64, (h - l).sqrt())
-                    .expect("Wasn't able to build normal distribution"))
-                .collect(),
             learning_cognitive,
             learning_social,
             inertia,
             best: vec![0f64; n],
+            best_fitness: std::f64::MIN,
             bounds,
-            particles
+            particles,
+            fitness_fn
         }
     }
 
-    pub fn update_velocity(&mut self){
+    pub fn update_particles(&mut self){
 
         let mut rng = rand::thread_rng();
 
+        let dim = self.best.len();
+
+        let mut best_in_run_fitness = std::f64::MIN;
+        let mut best_in_run_position = vec![0f64; dim]; 
+
+        // update particles
         for particle in self.particles.iter_mut() {
-            let mut velocity = vec![0f64; particle.velocity.len()];
+            let mut velocity = vec![0f64; dim];
             for (i, x_i) in particle.pos.iter_mut().enumerate() {
                 let c1r1:f64 = 
-                    self.learning_cognitive * self.normal_distribution[i].sample(&mut rng);
+                    self.learning_cognitive * rng.gen::<f64>();
                 let c2r2:f64 = 
-                    self.learning_social * self.normal_distribution[i].sample(&mut rng);
+                    self.learning_social * rng.gen::<f64>();
 
                 velocity[i] = self.inertia * particle.velocity[i]
-                    + c1r1 * (particle.p_best[i] - *x_i)  
+                    + c1r1 * (particle.best[i] - *x_i)  
                     + c2r2 * (self.best[i] - *x_i);
 
                 *x_i += velocity[i];
             }
             particle.velocity = velocity;
+
+            // calc particle fitness 
+            let particle_fitness = (self.fitness_fn)(&particle.pos);
+            particle.update_best(particle_fitness);
+
+            // update best in run
+            if best_in_run_fitness < particle_fitness {
+                best_in_run_position = particle.pos.clone();
+                best_in_run_fitness = particle_fitness;
+            }
+        }
+
+        // update swarm best
+        if self.best_fitness < best_in_run_fitness {
+            self.best = best_in_run_position;
+            self.best_fitness = best_in_run_fitness;
         }
     }
 }
 
 #[derive(Debug)]
 pub struct Particle {
-    pub p_best: Vec<f64>,
+    pub best: Vec<f64>,
+    pub best_fitness: f64,
     pub pos: Vec<f64>,
     pub velocity: Vec<f64> 
+    
 }
 
 impl Particle {
 
-    pub fn new_rnd(len: usize, distribution: &[Uniform<f64>]) -> Particle {
+    pub fn new_rnd(distribution: &[Uniform<f64>]) -> Particle {
+        let len = distribution.len();
         let mut rng = rand::thread_rng();
         Particle::new(vec![0f64; len].iter().enumerate()
             .map(|(i, _)| distribution[i].sample(&mut rng)).collect())
@@ -89,11 +124,70 @@ impl Particle {
     pub fn new(initial_pos: Vec<f64>) -> Particle {
         let l = initial_pos.len();
         Particle {
-            p_best: initial_pos.clone(),
+            best: initial_pos.clone(),
+            best_fitness: std::f64::MIN,
             pos: initial_pos,
             velocity: vec![0f64; l] 
         }
         
     }
+
+    pub fn update_best(&mut self, fitness: f64) {
+        if self.best_fitness < fitness {
+            self.best_fitness = fitness;
+            self.best = self.pos.clone();
+        }
+    }
+}
+
+/**
+ * Checks if x dominates y in pareto optimal sense. 
+ */
+pub fn dominates(x: &[f64], y: &[f64], directions: &[bool]) -> bool {
+    let mut all_at_least_equal_or_better = true;
+    let mut dominating_dimension_found = false;
+
+    for (i, x_i) in x.iter().enumerate() {
+        // \exists x_i : x_i > y_i 
+        if (directions[i] && *x_i > y[i])
+            || (!directions[i] && *x_i < y[i]) {
+
+            dominating_dimension_found = true;
+        }
+
+        // \forall x_i : x_i >= y_i
+        // negated to ! \exists x_i : x_i < y_i
+        if (directions[i] && *x_i < y[i]) 
+            || (!directions[i] && *x_i > y[i]) {
+
+            all_at_least_equal_or_better = false;
+            // we can short circuit here (negation of \forall)
+            break;
+        }
+    }
+
+    all_at_least_equal_or_better && dominating_dimension_found
+}
+
+pub fn pareto_front(xs: &[Vec<f64>], directions: &[bool]) -> Vec<usize> {
+    let len = xs.len();
+    let mut pareto_front = Vec::new();
+    
+    for i in 0..len {
+        let mut is_dominated = false;
+        let p = &xs[i];
+        for j in i+1..len {
+            if dominates(&xs[j], &p, directions) {
+                is_dominated = true;
+                break;
+            }
+        }
+
+        if !is_dominated {
+            pareto_front.push(i);
+        }
+    }
+
+    pareto_front
 }
 
--- src/pso_tests.rs
+++ src/pso_tests.rs
@@ -1,19 +1,57 @@
-use super::pso::{Particle, Swarm};
+use super::pso::{pareto_front, dominates, Particle, Swarm};
 use rand::random;
+use std::fs::write;
+
+// #[test]
+// fn test_update_velocity(){
+//     let len = 2;
+//     let mut swarm = Swarm::new (
+//         2,
+//         vec![
+//         (-100f64,100f64),
+//         (-100f64,100f64)
+//         ],
+//         1.2f64,
+//         1.2f64,
+//         2f64);
+//     println!("{:?}", swarm);
+//     swarm.update_velocity();
+//     println!("{:?}", swarm);
+// }
+
+fn points_to_string(points: &[Vec<f64>]) -> String {
+    points.iter()
+        .map(|xs| format!("{}\t{}", xs[0], xs[1]))
+        .collect::<Vec<String>>().join("\n")
+}
 
 #[test]
-fn test_update_velocity(){
-    let len = 2;
-    let mut swarm = Swarm::new (
-        2,
-        vec![
-        (-100f64,100f64),
-        (-100f64,100f64)
-        ],
-        1.2f64,
-        1.2f64,
-        2f64);
-    println!("{:?}", swarm);
-    swarm.update_velocity();
-    println!("{:?}", swarm);
+fn test_pareto_front() {
+    // testing pareto front on sine
+    let f_x: fn(f64) -> f64 = |x| x.sin() / x * 100.0;
+
+    let len = 10000;
+    let mut points: Vec<Vec<f64>> = Vec::with_capacity(len);
+    let interval = (0f64, 30f64);
+    let sampling_rate: f64 = (interval.1 - interval.0) / len as f64; 
+
+    for i in 0..len {
+        let x = interval.0 + i as f64 * sampling_rate;
+        points.push(vec![x, f_x(x)]);
+    }
+
+    write("domination_base.dat", points_to_string(&points)).unwrap();
+
+    let pareto_front = pareto_front(&points, &[true, false]);
+    let pareto_front: Vec<Vec<f64>> = pareto_front.iter()
+        .map(|i| points[*i].clone()).collect();
+
+    write("domination_front.dat", points_to_string(&pareto_front)).unwrap();
 }
+
+// #[test]
+// fn text_random_float(){
+//     for _ in 0..100 {
+//         println!("{}", rand::random::<f64>());
+//     }
+// }
