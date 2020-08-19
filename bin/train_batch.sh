beebfa3b0927a69ab7476641594ee56cb732cf29
Michael Welt <michael.welt@uni-ulm.de>
2020-08-19 14:48:36 +0200

    training batch modified

--- bin/train_batch.sh
+++ bin/train_batch.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env sh
 
 INPUT_FILE=countries.txt
-OUTPUT_DIR=pso_f1_wp_10k_100_50_5_batch
+OUTPUT_DIR=pso_f1_wp_10k_100_50_11_batch
 
 NITER=100
 NPARTICLES=50
@@ -10,7 +10,18 @@ NBWORDS=5
 BIN_FILES=~/data/wikipedia/bin_10k/
 
 run_train() {
-  $(env RUST_LOG=info \
+
+  # $(env RUST_LOG=info \
+  #   ./target/release/rsrelex \
+  #   --train $INPUT_FILE \
+  #   --to $OUTPUT_DIR/$1.dat \
+  #   -b ~/data/wikipedia/bin_10k/ \
+  #   --tniter $NITER \
+  #   --tnparticles $NPARTICLES \
+  #   --tnbwords $NBWORDS 
+  # )
+
+  $(cat countries_seed.txt | tail -n8 | env RUST_LOG=info \
     ./target/release/rsrelex \
     --train $INPUT_FILE \
     --to $OUTPUT_DIR/$1.dat \
@@ -21,7 +32,7 @@ run_train() {
   )
 }
 
-for i in $(seq 11 100) 
+for i in $(seq 1 50) 
 do
   run_train $i 
 done
